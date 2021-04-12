033268d2c474eaa0030fc3c90c1435fe2066fb03
MLLP <82297604+MLLP@users.noreply.github.com>
2021-04-12 01:27:07 -0300

    Add files via upload

--- CPA-CPCA.py
+++ CPA-CPCA.py
@@ -1,5 +1,15 @@
 #CPA e CPCA
+import numpy as np
+
+#Constantes
+par={"R":8.3144598, "Nav":6.02214076e23, "vk":[0.48, 1.574, -0.176], "c1":0.11, "Tc":650} 
+
+def alfaT(T, par):
+    Tr = T / par["Tc"]
+    return (1 + par["c1"] * (1 - np.sqrt(Tr)))**2
+
 def Z(rho, T, x, par):
     return 1.51
 
 Ztest = 1.0101
+print(par["R"], alfaT(300,par))
